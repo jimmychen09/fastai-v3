bcb8ee1d3119c32f5e8880b7fee7a12552595a22
Jimmy <jimmychen09@hotmail.com>
2019-01-27 19:54:20 +1100

    supdating file server.py

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1SHC27w05UL9lVyDi4xPFh1txw6FyANwf'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['akiec', 'bcc', 'blk', 'df', 'mel', 'nv', 'vasc']
 path = Path(__file__).parent
 
 app = Starlette()
