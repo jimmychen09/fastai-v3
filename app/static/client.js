43e86db47d7c1e43694b248b58b9e408b3f76c7e
Jimmy Chen <jimmychen09@hotmail.com>
2019-01-31 19:50:56 +1100

    minor changes

--- app/static/client.js
+++ app/static/client.js
@@ -1,5 +1,15 @@
 var el = x => document.getElementById(x);
 
+var dict = {
+    "akiec": "actinic keratosis",
+    "bcc": "basal cell carcinoma",
+    "blk": "benign keratosis",
+    "df": "dermatofibroma",
+    "mel": "melanocytic nevi",
+    "nv": "melanoma",
+    "vasc": "vascular"
+}
+
 function showPicker(inputId) { el('file-input').click(); }
 
 function showPicked(input) {
@@ -14,9 +24,9 @@ function showPicked(input) {
 
 function analyze() {
     var uploadFiles = el('file-input').files;
-    if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
+    if (uploadFiles.length != 1) alert('Please select a file!');
 
-    el('analyze-button').innerHTML = 'Analyzing...';
+    el('analyze-button').innerHTML = 'Analysing...';
     var xhr = new XMLHttpRequest();
     var loc = window.location
     xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
@@ -24,9 +34,9 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
-            el('result-label').innerHTML = `Result = ${response['result']}`;
+            el('result-label').innerHTML = `This is an image of ${response['result']}`;
         }
-        el('analyze-button').innerHTML = 'Analyze';
+        el('analyze-button').innerHTML = 'Analyse';
     }
 
     var fileData = new FormData();
